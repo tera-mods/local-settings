1acd89e51c68946007a2ed663c77536394758bfb
pinkipi <yarnovax@gmail.com>
2020-02-03 19:14:51 -0500

    Refactor packets

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 module.exports = function LocalSettings(mod) {
 	mod.settings.$init({
-		version: 1,
+		version: 2,
 		defaults: {
 			ui: null,
 			chatOption: null
+		},
+		migrate(version) {
+			switch(version) {
+				case 1:
+					this.ui = this.ui.map(ui => ({
+						name: ui.name,
+						locked: Boolean(ui.unk),
+						x: ui.x,
+						y: ui.y
+					}))
+			}
 		}
 	})
 
 	// UI
 	mod.hook('C_REQUEST_CLIENT_UI_SETTING', 1, event => {
 		if(mod.settings.ui) {
-			mod.send('S_REPLY_CLIENT_UI_SETTING', 1, {
+			mod.send('S_REPLY_CLIENT_UI_SETTING', 2, {
 				accountId: event.accountId,
 				uis: mod.settings.ui
 			})
@@ -18,12 +29,13 @@ module.exports = function LocalSettings(mod) {
 		}
 	})
 
-	mod.hook('S_REPLY_CLIENT_UI_SETTING', 1, event => {
+	mod.hook('S_REPLY_CLIENT_UI_SETTING', 2, event => {
 		if(mod.settings.ui) return false
+		for(let ui in event.uis) ui.locked = Boolean(ui.locked)
 		mod.settings.ui = event.uis
 	})
 
-	mod.hook('C_SAVE_CLIENT_UI_SETTING', 1, event => {
+	mod.hook('C_SAVE_CLIENT_UI_SETTING', 2, event => {
 		mod.settings.ui = event.uis
 	})
 
